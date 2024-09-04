0590fae704b99e14cd717e098a53982267446afc
Darvy Yu <thedarvy@gmail.com>
2024-09-04 14:45:26 -0700

    add api for get inventory list with given warehouse id,also add warehouse name column to inventorylist

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import initKnex from "knex";
+import configuration from "./../knexfile.js"
 import * as warehouseController from '../controllers/warehouse-controllers.js'
 
 
 const router = express.Router();
+const knex = initKnex(configuration);
 
-
+router.use('/:id/inventories', async (req, res)=>{
+    try{
+        const inventoryList = await knex("inventories")
+        .where({ warehouse_id: req.params.id });
+        res.json(inventoryList);
+    } catch (err){
+        console.log(err);
+        res.json({message:`error getting inventory list given warehouse id:${req.params.id} from database`});
+    }
+});
 
 
 export default router;
