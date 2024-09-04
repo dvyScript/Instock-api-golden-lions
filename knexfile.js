beb17750fd8887703b1c21ec2d771dd7d2ff40f7
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-03 19:01:41 -0600

    Update the host in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 export default {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     user: process.env.DB_LOCAL_USER,
     password: process.env.DB_LOCAL_PASSWORD,
     database: process.env.DB_LOCAL_DBNAME,
