056010c8d77217089826803aa7ab800ebd73f601
Darvy Yu <thedarvy@gmail.com>
2024-09-04 14:55:34 -0700

    send 404 when warehouse id not found

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -3,7 +3,6 @@ import initKnex from "knex";
 import configuration from "./../knexfile.js"
 import * as warehouseController from '../controllers/warehouse-controllers.js'
 
-
 const router = express.Router();
 const knex = initKnex(configuration);
 
@@ -11,6 +10,12 @@ router.use('/:id/inventories', async (req, res)=>{
     try{
         const inventoryList = await knex("inventories")
         .where({ warehouse_id: req.params.id });
+
+        if (inventoryList.length === 0){
+            res.status(404).json({
+                message:`error getting inventory list, warehouse id: ${req.params.id} does not exist`
+            });
+        }
         res.json(inventoryList);
     } catch (err){
         console.log(err);
