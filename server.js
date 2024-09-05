124bea63a922ce0360448b9545f1db084f38d987
Mu Dah <dahmu24@gmail.com>
2024-09-05 16:33:11 -0600

    Solved conflict

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const app = express();
 const{ PORT } = process.env;
 
 app.use(cors());
-<<<<<<< HEAD
-app.use(express.json());
-=======
 app.use(express.json()); 
->>>>>>> dev
 
 app.use('/inventories', inventoryRouter);
 
