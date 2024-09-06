05e38b8c89f1faf220c8bf315171a5cd5292892f
Mu Dah <dahmu24@gmail.com>
2024-09-06 12:15:30 -0600

    Removed duplicated codes locally for now

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,14 +3,6 @@ import * as inventoryController from "../controllers/inventory-controllers.js";
 
 const router = express.Router();
 
-const knex = initKnex(configuration);
-
-router
-    .get("/", inventoryController.index);
-
-router
-    .post("/", inventoryController.createInventoryItem);
-
 router.get("/", inventoryController.index);
 router.post("/", inventoryController.createInventoryItem);
 router.delete("/:inventoryId", inventoryController.deleteInventoryById);
